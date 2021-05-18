7a137c6ad75f0c99be23abad09918c68a6bd2484
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-18 17:58:41 +0300

    sad

---
+++ src/Assesment.kt
@@ -0,0 +1,37 @@
+fun main(){
+square()
+    var total=Truck("KAM",50,72)
+
+
+    var difference=Truck("KAM",56,72)
+
+}
+data class Book(var title:String,var author:String,var pages:Int)
+  fun listofBooks(listof()
+
+
+
+fun square(){
+
+    for ()
+        if (a <=-3000){
+
+            println(a*a)
+        }
+}
+class  Truck(var regitration:String,var totalWeight:Int,var capacity:Int){
+    fun load(weight:Int){
+        var totalWeight=totalWeight+capacity
+
+    }
+
+fun unload(weight: Int){
+   var totalWeight=totalWeight-capacity
+
+
+
+
+}
+
+}
+
